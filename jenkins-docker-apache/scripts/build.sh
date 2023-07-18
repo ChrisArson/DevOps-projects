d0e666b6b0130a827c35b2d77bc7c0c868d9da04
ChrisArson <arsoon97@gmail.com>
2023-07-18 21:21:25 +0000

    changed paths in jenkisfile

--- jenkins-docker-apache/scripts/build.sh
+++ jenkins-docker-apache/scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker compose -f web/docker-compose-alpine.yaml up -d --force-recreate
+docker compose -f $PWD/web/docker-compose-alpine.yaml up -d --force-recreate
 docker cp alpine-web:/mnt/webfiles/data data/
 sudo rm -rf data/webfiles
 sudo mv data/data data/webfiles

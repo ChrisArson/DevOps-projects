ad2ce8278d6a321ce0e19adf720705375fe5c204
ChrisArson <arsoon97@gmail.com>
2023-07-18 21:53:35 +0000

    changes permissions to files

--- jenkins-docker-apache/scripts/backup_vm.sh
+++ jenkins-docker-apache/scripts/backup_vm.sh
@@ -4,6 +4,8 @@ hosts="$PWD/jenkins-docker-apache/data/ip.txt"
 priv_key="$PWD/jenkins-docker-apache/data/id_rsa"
 script="$PWD/jenkins-docker-apache/scripts/vm/backup.sh"
 
+chmod 600 "$priv_key"
+
 while IFS= read -r ip_address
 do
     scp -i "$priv_key" "$script" "vagrant@$ip_address:/tmp"

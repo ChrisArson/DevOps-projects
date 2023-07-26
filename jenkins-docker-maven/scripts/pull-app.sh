a396d13229765f39f6a81a65f00ebaf7bfc02803
ChrisArson <arsoon97@gmail.com>
2023-07-26 15:10:43 +0000

    updated Jenkinsfile with maven tool

--- jenkins-docker-maven/scripts/pull-app.sh
+++ jenkins-docker-maven/scripts/pull-app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/jenkins-docs/simple-java-maven-app.git
+mkdir -p app
 cp -r simple-java-maven-app/jenkins simple-java-maven-app/pom.xml simple-java-maven-app/src app 
 rm -rf simple-java-maven-app

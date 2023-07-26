360ffc3fde7a5344db463a86c9ed2dbd0882b639
ChrisArson <arsoon97@gmail.com>
2023-07-26 16:35:36 +0000

    changing owner of target dir

--- jenkins-docker-maven/scripts/build-app.sh
+++ jenkins-docker-maven/scripts/build-app.sh
@@ -2,5 +2,6 @@
 
 docker run --rm -v /home/vagrant/projects/jenkins-docker-maven/jenkins_backup_data/jenkins_home/workspace/jenkins-docker-maven/jenkins-docker-maven/app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.6-eclipse-temurin-11-alpine mvn -B -DskipTests clean package
 
+chown -r 1000:1000 $PWD/jenkins-docker-maven/app/target
 cp -r $PWD/jenkins-docker-maven/app/target/*.jar $PWD/jenkins-docker-maven/data
 

05f7741caa1da6e1415e72002d79afa7383c4a44
ChrisArson <arsoon97@gmail.com>
2023-07-26 15:45:19 +0000

    testing paths

--- jenkins-docker-maven/scripts/pull-app.sh
+++ jenkins-docker-maven/scripts/pull-app.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/jenkins-docs/simple-java-maven-app.git
 mkdir -p app
 cp -r simple-java-maven-app/jenkins simple-java-maven-app/pom.xml simple-java-maven-app/src app 
-rm -rf simple-java-maven-app
+#rm -rf simple-java-maven-app

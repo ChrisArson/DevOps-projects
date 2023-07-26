889895d2364fd866bb598c775fd860a16cd4327d
ChrisArson <arsoon97@gmail.com>
2023-07-26 16:00:17 +0000

    fixed paths

--- jenkins-docker-maven/scripts/pull-app.sh
+++ jenkins-docker-maven/scripts/pull-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WORK_PATH="/home/vagrant/projects/jenkins-docker-maven/jenkins_backup_data/jenkins_home/workspace/jenkins-docker-maven/jenkins-docker-maven"
+WORK_PATH="/home/vagrant/projects/jenkins-docker-maven/jenkins_backup_data/jenkins_home/workspace/jenkins-docker-maven"
 git clone https://github.com/jenkins-docs/simple-java-maven-app.git "$WORK_PATH"
 mkdir -p "$WORK_PATH/app"
 cp -r "$WORK_PATH/simple-java-maven-app/jenkins" "$WORK_PATH/simple-java-maven-app/pom.xml" "$WORK_PATH/simple-java-maven-app/src" "$WORK_PATH/app" 

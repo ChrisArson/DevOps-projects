7850986a20e5e5216c3165d8118615325693ddce
ChrisArson <arsoon97@gmail.com>
2023-07-26 15:27:06 +0000

    changed paths

--- jenkins-docker-maven/scripts/build-app.sh
+++ jenkins-docker-maven/scripts/build-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -v /home/vagrant/projects/jenkins-docker-maven/simple-java-maven-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.6-eclipse-temurin-11-alpine mvn -B -DskipTests clean package
+docker run --rm -v $PWD/jenkins-docker-maven/app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.6-eclipse-temurin-11-alpine mvn -B -DskipTests clean package
 
-cp -r app/target/*.jar data
+cp -r $PWD/app/target/*.jar $PWD/data
 

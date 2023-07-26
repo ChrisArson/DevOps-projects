d98777f4dc313783b2468a8f76ca10af64d3a9c3
ChrisArson <arsoon97@gmail.com>
2023-07-26 14:41:07 +0000

    deleted sudo from script

--- jenkins-docker-maven/scripts/pull-app.sh
+++ jenkins-docker-maven/scripts/pull-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/jenkins-docs/simple-java-maven-app.git
-sudo cp -r simple-java-maven-app/jenkins simple-java-maven-app/pom.xml simple-java-maven-app/src app 
-sudo rm -rf simple-java-maven-app
+cp -r simple-java-maven-app/jenkins simple-java-maven-app/pom.xml simple-java-maven-app/src app 
+rm -rf simple-java-maven-app

5adb7b6d95fa4e2019fd08146f47015970ce404f
ChrisArson <arsoon97@gmail.com>
2023-07-26 16:15:57 +0000

    testing paths

--- jenkins-docker-maven/scripts/build-app.sh
+++ jenkins-docker-maven/scripts/build-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -v $PWD/jenkins-docker-maven/app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.6-eclipse-temurin-11-alpine mvn -B -DskipTests clean package
+docker run --rm -v "${PWD}/jenkins-docker-maven/app":"/app" -v /root/.m2/:/root/.m2/ -w /app maven:3.8.6-eclipse-temurin-11-alpine mvn -B -DskipTests clean package
 
 #cp -r $PWD/jenkins-docker-maven/app/target/*.jar $PWD/jenkins-docker-maven/data
 

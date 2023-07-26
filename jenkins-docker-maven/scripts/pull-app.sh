6509e1131bebee0a856fdaa6a8bfcf34bddc9afd
ChrisArson <arsoon97@gmail.com>
2023-07-26 16:07:55 +0000

    testing paths

--- jenkins-docker-maven/scripts/pull-app.sh
+++ jenkins-docker-maven/scripts/pull-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #WORK_PATH="/home/vagrant/projects/jenkins-docker-maven/jenkins_backup_data/jenkins_home/workspace/jenkins-docker-maven"
-git clone https://github.com/jenkins-docs/simple-java-maven-app.git "$PWD/jenkins-docker-maven"
+git clone https://github.com/jenkins-docs/simple-java-maven-app.git
 mkdir -p "$PWD/jenkins-docker-maven/app"
-cp -r "$WORK_PATH/simple-java-maven-app/jenkins" "$WORK_PATH/simple-java-maven-app/pom.xml" "$WORK_PATH/simple-java-maven-app/src" "$WORK_PATH/app" 
+cp -r "$PWD/simple-java-maven-app/jenkins" "$PWD/simple-java-maven-app/pom.xml" "$PWD/simple-java-maven-app/src" "$PWD/jenkins-docker-maven/app" 
 #rm -rf simple-java-maven-app

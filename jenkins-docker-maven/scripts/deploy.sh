1251fe9f4523d39e40c48087c37089706b789d43
ChrisArson <arsoon97@gmail.com>
2023-07-27 15:56:37 +0000

    testing connection in deploy.sh

--- jenkins-docker-maven/scripts/deploy.sh
+++ jenkins-docker-maven/scripts/deploy.sh
@@ -5,9 +5,12 @@ script="$PWD/jenkins-docker-maven/scripts/vm/init.sh"
 
 chmod 600 "$priv_key"
 
+
+#scp -i "$priv_key" "$script" "vagrant@192.168.77.20:/tmp"
+ssh -v -i "$priv_key" "vagrant@192.168.77.20"
 #Copy script, webfiles and execute script on VMs
-while IFS= read -r ip_address
-do
-    scp -i "$priv_key" "$script" "vagrant@$ip_address:/tmp"
-    #ssh -i "$priv_key" -n "vagrant@$ip_address" "/tmp/init.sh $1 $2 $3" < /dev/null
-done < "$hosts"
+#while IFS= read -r ip_address
+#do
+#    scp -i "$priv_key" "$script" "vagrant@$ip_address:/tmp"
+#    ssh -i "$priv_key" -n "vagrant@$ip_address" "/tmp/init.sh $1 $2 $3" < /dev/null
+#done < "$hosts"

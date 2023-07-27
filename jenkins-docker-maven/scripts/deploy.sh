dd1f440bf5fccd9f52913d8e66c7c9962e5166e3
ChrisArson <arsoon97@gmail.com>
2023-07-27 15:48:37 +0000

    commented ssh in deploy.sh for tests

--- jenkins-docker-maven/scripts/deploy.sh
+++ jenkins-docker-maven/scripts/deploy.sh
@@ -9,5 +9,5 @@ chmod 600 "$priv_key"
 while IFS= read -r ip_address
 do
     scp -i "$priv_key" "$script" "vagrant@$ip_address:/tmp"
-    ssh -i "$priv_key" -n "vagrant@$ip_address" "/tmp/init.sh $1 $2 $3" < /dev/null
+    #ssh -i "$priv_key" -n "vagrant@$ip_address" "/tmp/init.sh $1 $2 $3" < /dev/null
 done < "$hosts"

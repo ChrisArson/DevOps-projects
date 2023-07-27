85268afcbc7fad18b414c96c2a80b6329b9bf4de
ChrisArson <arsoon97@gmail.com>
2023-07-27 15:20:18 +0000

    added chmod to deploy.sh

--- jenkins-docker-maven/scripts/deploy.sh
+++ jenkins-docker-maven/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#hosts="$PWD/jenkins-docker-maven/data/ip.txt"
 hosts="$PWD/jenkins-docker-maven/data/ip.txt"
-#priv_key="$PWD/jenkins-docker-maven/data/id_rsa"
 priv_key="$PWD/jenkins-docker-maven/data/id_rsa"
-#script="$PWD/jenkins-docker-maven/scripts/vm/init.sh"
 script="$PWD/jenkins-docker-maven/scripts/vm/init.sh"
+
+chmod 600 "$priv_key"
 
 #Copy script, webfiles and execute script on VMs
 while IFS= read -r ip_address
